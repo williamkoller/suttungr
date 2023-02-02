2e09b80d8ade184eee60ecbd6ec72d86edb59a0c
William Koller <williamkoller404@gmail.com>
2023-02-02 15:49:16 -0300

    test: :white_check_mark: add new tests in EnvironmentService

--- src/infra/config/environment/environment.service.spec.ts
+++ src/infra/config/environment/environment.service.spec.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { EnvironmentService } from './environment.service';
 
@@ -6,7 +7,7 @@ describe('EnvironmentService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EnvironmentService],
+      providers: [EnvironmentService, ConfigService],
     }).compile();
 
     service = module.get<EnvironmentService>(EnvironmentService);
@@ -14,5 +15,39 @@ describe('EnvironmentService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be getJwtSecret', () => {
+    const environmentService = service.getJwtSecret();
+    jest
+      .spyOn(service, 'getJwtSecret')
+      .mockImplementation(() => environmentService);
+    expect(service.getJwtSecret()).toEqual(environmentService);
+  });
+
+  it('should be getJwtRefreshToken', () => {
+    const environmentService = service.getJwtRefreshToken();
+    jest
+      .spyOn(service, 'getJwtRefreshToken')
+      .mockImplementation(() => environmentService);
+    expect(service.getJwtRefreshToken()).toEqual(environmentService);
+  });
+
+  it('should be getExpiresIn', () => {
+    const environmentService = service.getExpiresIn();
+    jest
+      .spyOn(service, 'getExpiresIn')
+      .mockImplementation(() => environmentService);
+    expect(service.getExpiresIn()).toEqual(environmentService);
+  });
+
+  it('should be getJwtRefreshTokenExpirationTime', () => {
+    const environmentService = service.getJwtRefreshTokenExpirationTime();
+    jest
+      .spyOn(service, 'getJwtRefreshTokenExpirationTime')
+      .mockImplementation(() => environmentService);
+    expect(service.getJwtRefreshTokenExpirationTime()).toEqual(
+      environmentService,
+    );
   });
 });
